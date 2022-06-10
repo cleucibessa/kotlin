fc309701547016da2d2b864f467af263879d95e2
Barreto <cleuci.24@gmail.com>
2022-06-09 21:11:58 -0400

    Initial commit

---
+++ soma.kt
@@ -0,0 +1,19 @@
+    fun soma (x:Int, y:Int): Int {
+        return x+y
+    }
+    fun subtracao (x:Int, y:Int): Int {
+        return x-y
+    }
+    fun main(){
+        println ("Soma de dois números")
+        println (soma (4,3))
+        println("Subtração de dois númeors")
+        println(subtracao(4,0))
+    }
+
+
+
+
+
+
+    
